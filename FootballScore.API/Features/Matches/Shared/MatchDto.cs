760fca520c2b2c6c0aee338ffa663abf432dc6bc
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-12-10 14:57:34 +0200

    Make DTO for the football matches

---
+++ FootballScore.API/Features/Matches/Shared/MatchDto.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace FootballScore.API.Features.Matches.Shared
+{
+    public class MatchDto
+    {
+        public int Id { get; set; }
+        public int HomeTeamId { get; set; }
+        public string HomeTeamName { get; set; } = string.Empty;
+
+        public int AwayTeamId { get; set; }
+        public string AwayTeamName { get; set; } = string.Empty;
+
+        public int HomeGoals { get; set; }
+        public int AwayGoals { get; set; }
+
+        public DateTime MatchDate { get; set; }
+    }
+}
--- FootballScore.API/obj/Debug/net5.0/FootballScore.API.AssemblyInfo.cs
+++ FootballScore.API/obj/Debug/net5.0/FootballScore.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FootballScore.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b4a7c524fa28ae625990ac4f6b38017f940070f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc7fb2ef6d94455dc8b4ab2893988c31c114a5cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FootballScore.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FootballScore.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
