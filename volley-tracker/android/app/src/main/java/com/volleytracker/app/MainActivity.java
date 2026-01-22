1f9090da59e26b2272da72737710bab793bade2d
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2026-01-22 13:03:03 +0200

    delete

--- volley-tracker/android/app/src/main/java/com/volleytracker/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.volleytracker.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- volley-tracker/src/app/core/models.ts
+++
@@ -1,5 +0,0 @@
-export interface Coach {
-    id: string;
-    name: string;
-    createdAt: string; // ISO date string
-}
