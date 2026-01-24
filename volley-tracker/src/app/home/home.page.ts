cab1be7ddf8ed31199b76ad1538942d240a212e7
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2026-01-24 10:00:02 +0200

    test the ionic/ios butoons

--- volley-tracker/src/app/home/home.page.ts
+++ volley-tracker/src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import {
+  IonHeader, IonToolbar, IonTitle, IonContent,
+  IonButton, IonActionSheet
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonActionSheet],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  constructor() {}
+  isOpen = false;
+
+  buttons = [
+    { text: 'Delete', role: 'destructive', handler: () => console.log('Delete') },
+    { text: 'Share', handler: () => console.log('Share') },
+    { text: 'Cancel', role: 'cancel' },
+  ];
+
+  openActions() {
+    this.isOpen = true;
+  }
 }
