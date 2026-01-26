f437b4073faabb461df6acbc8fe63f9ea5642274
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2026-01-26 09:43:59 +0200

    home page v1 upgrade

--- volley-tracker/src/app/app.routes.ts
+++ volley-tracker/src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+//import { SetupPage } from './setup/setup.page';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+  { path: '', component: HomePage },
+  //{ path: 'setup', component: SetupPage },
 ];
